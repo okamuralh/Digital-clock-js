55464aa8a36c4807f5ed5650cc2f709fa63af0fb
okamuralh <okamura.lh@gmail.com>
2022-02-05 10:10:56 +0000

    showTime function created

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+function showTime() {
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let amPm = "AM";
 
+  if (h === 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    amPm = "PM";
+  }
+
+  if (h < 10) {
+    h = `0${h}`;
+  }
+
+  if (m < 10) {
+    m = `0${m}`;
+  }
+
+  if (s < 10) {
+    s = `0${s}`;
+  }
+
+  let clock = document.querySelector("#my-clock");
+
+  clock.innerHTML = `${h}:${m}:${s} ${amPm}`;
+
+  setTimeout(showTime, 1000);
+}
+
+showTime();
